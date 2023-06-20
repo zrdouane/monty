de3f1ffaa5b74e656643448da5810d366f01f9e3
zrdouane <ramirdouane@gmail.com>
2023-06-20 12:57:48 +0100

    add function that Does nothing.

---
+++ nop.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+/**
+ * nop - Does nothing.
+ * @node: Pointer to a pointer to the top of the stack.
+ * @compteur: Current line number.
+ */
+void nop(_stack_t **node, unsigned int compteur)
+{
+	(void)node;
+	(void)compteur;
+}

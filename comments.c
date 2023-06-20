c342bc1021c31fd79a557b095aacb5af09c3bcfc
zrdouane <ramirdouane@gmail.com>
2023-06-20 13:01:19 +0100

    add function that Does nothing when type #.

---
+++ comments.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+/**
+ * comments - Does nothing.
+ * @node: Pointer to a pointer to the top of the stack.
+ * @compteur: Current line number.
+ */
+void comments(_stack_t **node, unsigned int compteur)
+{
+	(void)node;
+	(void)compteur;
+}
